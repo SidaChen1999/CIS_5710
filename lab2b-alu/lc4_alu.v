475cfd8aa58debc7b022fe834a8547c61966a467
Star Chen <chensida@seas.upenn.edu>
2023-02-05 15:26:42 -0500

    ALU schematic & CLA.c

--- lab2b-alu/lc4_alu.v
+++ lab2b-alu/lc4_alu.v
@@ -1,4 +1,6 @@
 /* INSERT NAME AND PENNKEY HERE */
+// Sida Chen, chensida
+// Jingyi Chen, cjychen
 
 `timescale 1ns / 1ps
 `default_nettype none
@@ -11,5 +13,22 @@ module lc4_alu(input  wire [15:0] i_insn,
 
 
       /*** YOUR CODE HERE ***/
+      wire [15:0] output_temp [15:0];
+      assign o_result = (i_insn[15:12] == 4'b0000) ? output_temp[0] :
+                        (i_insn[15:12] == 4'b0001) ? output_temp[1] :
+                        (i_insn[15:12] == 4'b0010) ? output_temp[2] :
+                        (i_insn[15:12] == 4'b0011) ? output_temp[3] :
+                        (i_insn[15:12] == 4'b0100) ? output_temp[4] :
+                        (i_insn[15:12] == 4'b0101) ? output_temp[5] :
+                        (i_insn[15:12] == 4'b0110) ? output_temp[6] :
+                        (i_insn[15:12] == 4'b0111) ? output_temp[7] :
+                        (i_insn[15:12] == 4'b1000) ? output_temp[8] :
+                        (i_insn[15:12] == 4'b1001) ? output_temp[9] :
+                        (i_insn[15:12] == 4'b1010) ? output_temp[10] :
+                        (i_insn[15:12] == 4'b1011) ? output_temp[11] :
+                        (i_insn[15:12] == 4'b1100) ? output_temp[12] :
+                        (i_insn[15:12] == 4'b1101) ? output_temp[13] :
+                        (i_insn[15:12] == 4'b1110) ? output_temp[14] : output_temp[15];
+      
 
 endmodule
--- lab2b-alu/lc4_cla.v
+++ lab2b-alu/lc4_cla.v
@@ -1,4 +1,6 @@
 /* TODO: INSERT NAME AND PENNKEY HERE */
+// Sida Chen, chensida
+// Jingyi Chen, cjychen
 
 `timescale 1ns / 1ps
 `default_nettype none
@@ -28,7 +30,13 @@ module gp4(input wire [3:0] gin, pin,
            input wire cin,
            output wire gout, pout,
            output wire [2:0] cout);
-   
+
+  assign pout = & pin;
+  assign gout = gin[3] | pin[3]&gin[2] | (&pin[3:2])&gin[1] | (&pin[3:1])&gin[0];
+  assign cout[0] = gin[0] | pin[0]&cin;
+  assign cout[1] = gin[1] | pin[1]&gin[0] | (&pin[1:0])&cin;
+  assign cout[2] = gin[2] | pin[2]&gin[1] | (&pin[2:1])&gin[0] | (&pin[2:0])&cin;
+
 endmodule
 
 /**
@@ -43,6 +51,28 @@ module cla16
    input wire         cin,
    output wire [15:0] sum);
 
+  wire [15:0] g;
+  wire [15:0] p;
+  wire [15:0] c;
+  assign c[0] = cin;
+  wire [2:0] c_inter;
+  wire [3:0] g_inter;
+  wire [3:0] p_inter;
+  wire g15, p15;
+  wire [15:0] sum_inter = a ^ b;
+  genvar i;
+  for (i = 0; i < 16; i = i+1) begin
+    gp1 d1(.a(a[i]), .b(b[i]), .g(g[i]), .p(p[i]));
+  end
+  genvar j;
+  for (j = 0; j < 4; j = j+1) begin
+    gp4 d3(.gin(g[3+4*j:4*j]), .pin(p[3+4*j:4*j]), .cin(c[j*4]), 
+           .gout(g_inter[j]), .pout(p_inter[j]), .cout(c[3+j*4:1+j*4]));
+  end
+  gp4 d5(.gin(g_inter), .pin(p_inter), .cin(c[0]), 
+           .gout(g15), .pout(p15), .cout({c[12], c[8], c[4]})); // c[2:0], MSB at left
+  assign sum = c ^ sum_inter;
+
 endmodule
 
 
@@ -56,5 +86,34 @@ module gpn
    input wire  cin,
    output wire gout, pout,
    output wire [N-2:0] cout);
+
+  assign pout = & pin;
+
+  // wire [N-1:0] gout_out;
+  wire [N-1:0] gout_inter [N-1:0];
+  wire [N-1:0] cout_inter [N-1:0];
+  genvar i;
+  genvar j;
+  genvar k;
+  for (i = N-1; i >= 0; i = i-1) begin
+      assign gout_inter[i][i] = gin[N-1-i];
+      for (j = i-1; j >= 0; j = j-1) begin
+        assign gout_inter[i][j] = gout_inter[i][j+1] & pin[j+N-i];
+
+    end
+  end
+  assign gout = | gout_inter[0];
+
+  for (k = 0; k < N-1; k = k+1) begin
+    for (i = N-1; i >= 0; i = i-1) begin
+      if (i == k) assign cout_inter[i][i] = cin;
+      else assign cout_inter[i][i] = gin[N-1-i];
+      for (j = i-1; j >= 0; j = j-1) begin
+        assign cout_inter[i][j] = cout_inter[i][j+1] & pin[j+N-i];
+
+      end
+    end
+    assign cout[k] = | cout_inter[0];
+  end
  
 endmodule
