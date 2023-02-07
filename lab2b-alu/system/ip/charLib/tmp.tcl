0e65b1af2f1d6ce7cfb003910f0134732f016d79
Sida Chen <chensida@seas.upenn.edu>
2023-02-07 18:17:34 -0500

    Revert "ALU implmented, potential fix of IP core error during impl"
    This reverts commit 88928562316e14c5e746775f18504482c0723610.

--- lab2b-alu/system/ip/charLib/tmp.tcl
+++
@@ -1,4 +0,0 @@
-create_project -in memory
-set_property part xc7z020clg484-1 [current_project]
-read_ip charLib.xci
-upgrade_ip [get_ips charLib]
--- lab2b-alu/system/ip/pixel_buffer/tmp.tcl
+++
@@ -1,4 +0,0 @@
-create_project -in memory
-set_property part xc7z020clg484-1 [current_project]
-read_ip pixel_buffer.xci
-upgrade_ip [get_ips pixel_buffer]
