b0fc34eff4824b21a03f190312090d9fa34443a1
jychen <chen2022.p2@gmail.com>
2023-04-12 20:32:47 -0400

    superscalar waiting for debug

--- lab5-superscalar/lc4_divider.v
+++ lab5-superscalar/lc4_divider.v
@@ -9,6 +9,25 @@ module lc4_divider(input  wire [15:0] i_dividend,
                    output wire [15:0] o_quotient);
 
       /*** YOUR CODE HERE ***/
+      wire [15:0] dividend [16:0];
+      wire [15:0] quotient [16:0];
+      wire [15:0] remainder [16:0];
+      assign dividend[0] = i_dividend;
+      assign remainder[0] = 16'h0000;
+      assign quotient[0] = 16'h0000;
+      genvar i;
+      for (i = 0; i < 16; i = i+1) begin
+            lc4_divider_one_iter iter(    .i_dividend(dividend[i]),
+                                          .i_divisor(i_divisor),
+                                          .i_remainder(remainder[i]),
+                                          .i_quotient(quotient[i]),
+                                          .o_dividend(dividend[i+1]),
+                                          .o_remainder(remainder[i+1]),
+                                          .o_quotient(quotient[i+1])
+                                          );
+      end
+      assign o_remainder = (i_divisor > 0) ? remainder[16] : 16'h0000;
+      assign o_quotient = (i_divisor > 0) ? quotient[16] : 16'h0000;
 
 endmodule // lc4_divider
 
@@ -21,5 +40,9 @@ module lc4_divider_one_iter(input  wire [15:0] i_dividend,
                             output wire [15:0] o_quotient);
 
       /*** YOUR CODE HERE ***/
+      wire [15:0] remainder = (i_remainder << 1) | ((i_dividend >> 15) & 1'b1);
+      assign o_quotient = (remainder < i_divisor) ? i_quotient << 1 : (i_quotient << 1) | 1'b1;
+      assign o_remainder = (remainder < i_divisor) ? remainder: remainder - i_divisor;
+      assign o_dividend = i_dividend << 1;
 
 endmodule
